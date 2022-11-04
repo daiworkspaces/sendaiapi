84ced3e821da2b12420a24b002d067b30c12154e
chaomingdai <xmcb008@sina.com>
2022-11-04 21:59:56 +0800

    实现get cookies请求

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesGet.java
@@ -1,29 +1,21 @@
 package com.course.httpclient.cookies;
 
-import com.sun.org.apache.xml.internal.security.keys.storage.implementations.SingleCertificateResolver;
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.cookie.ClientCookie;
 import org.apache.http.cookie.Cookie;
-import org.apache.http.cookie.CookieSpecProvider;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.rmi.registry.Registry;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -32,7 +24,7 @@ public class MyCookiesGet {
     private String url ;
     private ResourceBundle bundle;
     //用来存储cookie信息
-    private CookieStore cookieStore;
+   private BasicCookieStore cookieStore;
 
 
 
@@ -47,117 +39,108 @@ public class MyCookiesGet {
         System.out.println(url);
     }
 
-    @Test
-    public void testGetCookies() throws IOException {
-        String result;
-        String uri = bundle.getString("getCookies.uri");
-        String testUrl = this.url +uri;
-        HttpGet get = new HttpGet(testUrl);
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        CloseableHttpResponse response = httpClient.execute(get);
-
-        HttpEntity entityUtils = response.getEntity();
-        result = EntityUtils.toString(entityUtils,"UTF-8");
-//        result = EntityUtils.toString(response.getEntity(),"UTF-8");
-        System.out.println(result);
-
-
-
-    }
 
     @Test
     public void  testCookieStore() throws IOException {
-        cookieStore = new BasicCookieStore();
-        CloseableHttpClient httpClient = null;
-        httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
+
+
         String url =this.url+ bundle.getString("getCookies.uri");
         HttpGet httpGet = new HttpGet(url);
 
-        CloseableHttpResponse response = httpClient.execute(httpGet);
-        HttpEntity entity = response.getEntity();
-        String result = EntityUtils.toString(entity);
+         this.cookieStore = new BasicCookieStore();
 
-        //获取cookie信息
-        List<Cookie> cookies = cookieStore.getCookies();
-//        for (int i =0; i<cookies.size();i++){
-//            System.out.println(cookies.get(i).getName());
-//            System.out.println(cookies.get(i).getValue());
-//        }
-
-        for (Cookie cookie :cookies){
-            String name = cookie.getName();
-            String value = cookie.getValue();
-            System.out.println(name);
-            System.out.println(value);
-        }
-
-
-        EntityUtils.consume(response.getEntity());
-    }
-
-
-    @Test(dependsOnMethods = {"testCookieStore"})
-    public void testDaiCookie() throws IOException {
-        CloseableHttpClient httpClient = null;
-         url = this.url + bundle.getString("daiCookies.uri");
-        System.out.println(url);
-
-        String cookieName=null;
-        String cookieValue =null;
-        List<Cookie> cookies = cookieStore.getCookies();
-        for (Cookie cookie :cookies){
-            cookieName = cookie.getName();
-            cookieValue = cookie.getValue();
-            System.out.println(cookieName);
-            System.out.println(cookieValue);
-        }
-
-        BasicClientCookie cookie = new BasicClientCookie(cookieName,cookieValue);
-        cookie.setDomain(cookie.getDomain());
-        cookie.setPath(cookie.getPath());
-        cookie.setAttribute(ClientCookie.PATH_ATTR,cookie.getPath());
-        cookie.setAttribute(ClientCookie.DOMAIN_ATTR,cookie.getDomain());
-        cookieStore.addCookie(cookie);
-
-        httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
         HttpClientContext context = HttpClientContext.create();
         context.setCookieStore(cookieStore);
 
 
-        RequestConfig requestConfig = RequestConfig.custom()
-                .setCookieSpec("singleCookiesSpec")
-                .build();
+        CloseableHttpClient client = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
 
-        context.setCookieSpecRegistry(context.getCookieSpecRegistry());
+        CloseableHttpResponse response = client.execute(httpGet,context);
+
+        HttpEntity entity = response.getEntity();
+        String result = EntityUtils.toString(entity);
+
+        //获取cookie信息
+        for (Cookie c : cookieStore.getCookies()){
+            System.out.println(c.getName());
+            System.out.println(c.getValue());
+        }
 
 
+        //EntityUtils.consume(response.getEntity());
+    }
 
 
+    @Test
+    public void testGetWihCookies() throws IOException {
+        String url = bundle.getString("daiCookies.uri");
+        String testUrl = this.url+url;
 
+        RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
 
-
-        HttpGet httpGet = new HttpGet(url);
-
-
+        HttpClientContext context = HttpClientContext.create();
+        
+        context.setCookieStore(cookieStore);
         System.out.println(cookieStore.toString());
 
-        //设置cookies信息
 
 
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(globalConfig)
+                .setDefaultCookieStore(cookieStore).build();
+        HttpGet httpGet = new HttpGet(testUrl);
+        httpGet.addHeader("Cookies","login=true");
+
+
+//        HttpClientContext context = new HttpClientContext();
+//        context.setCookieStore(cookieStore);
 
         CloseableHttpResponse response = httpClient.execute(httpGet,context);
         HttpEntity entity = response.getEntity();
         System.out.println(response.toString());
         String result = EntityUtils.toString(entity,"UTF-8");
-        System.out.println(result);
-        System.out.println("------------------");
+        System.out.println("打印结果2"+result);
+
+
+
+
+
+
 
     }
 
 
+    @Test
+    public void testGetCookies() throws IOException {
+
+        cookieStore = new BasicCookieStore();
+        String tesUrl = this.url + bundle.getString("cookiesDemo.uri");
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
+        HttpGet httpGet = new HttpGet(tesUrl);
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        String result = EntityUtils.toString(entity);
+        EntityUtils.consume(entity);
+        System.out.println(result);
+
+        System.out.println("cookie store: "+cookieStore.getCookies());
+
+    }
 
 
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testCookieDemo() throws IOException {
+        String url = this.url +bundle.getString("huoQuCookies.uri");
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore)
+                .build();
 
+        HttpGet  httpGet = new HttpGet(url);
+        httpGet.addHeader("Cookie",cookieStore.toString());
+
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        String result = EntityUtils.toString(response.getEntity());
+        System.out.println(result);
+
+    }
 
 }
