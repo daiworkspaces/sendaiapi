577bab61f3b6bf88371600e378446da086e5bde3
chaomingdai <xmcb008@sina.com>
2022-11-18 22:33:58 +0800

    添加mybatis与springboot实现更新与删除

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,13 +1,15 @@
 package com.course.controller;
 
+import com.course.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.ws.RequestWrapper;
 
 @Log4j2
 @RestController
@@ -21,12 +23,33 @@ public class Demo {
     private SqlSessionTemplate template;
 
 
-    @RequestMapping(value = "/getUSerCount",method = RequestMethod.GET)
+    @RequestMapping(value = "/getUSerCount",method = RequestMethod.GET,produces = {"text/plain;charset=UTF-8"})
     @ApiOperation(value = "获取到用户总数",httpMethod = "GET")
     public int getUSerCount(){
         return template.selectOne("getUserCount");
 
     }
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST,produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "添加用户",httpMethod = "POST")
+    public int addUser(@RequestBody User user){
+        return  template.insert("addUser",user);
+
+    }
+
+    @RequestMapping(value = "/updateUser",method = RequestMethod.POST,produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "更新用户信息",httpMethod = "POST")
+    public int updateUser(@RequestBody User user){
+        return template.update("updateUser",user);
+    }
+
+    //produces 的写法是为了解决删除问题返回500 但是数据库的数据已经被删除的问题
+    @RequestMapping(value = "/deleteUser",method = RequestMethod.GET,consumes = MediaType.ALL_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "删除用户信息",httpMethod = "GET")
+    public int delUser(@RequestParam int id){
+           return template.delete("deleteUser",id);
+    }
+
 
 
 }
